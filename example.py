a876c5cbe261022abc0d1589ed90c41a637f2349
Artem O. Volkov <volckov.tyoma@gmail.com>
2019-03-08 21:25:57 +0300

    kick off

---
+++ example.py
@@ -0,0 +1,39 @@
+from bitmart_api import *
+
+api_key = '<your api key>'
+secret_key = '<your secret key>'
+
+# title of your API key that you set when registered your key.
+title = '<title>'
+
+
+
+############# Usage #############
+
+bitmart = Bitmart(api_key, secret_key, title)
+
+# Public Endpoints
+
+print(bitmart.ping())
+print(bitmart.time())
+print(bitmart.steps())
+print(bitmart.currencies())
+print(bitmart.symbols())
+print(bitmart.symbols_details())
+print(bitmart.ticker('ETH_BTC'))
+print(bitmart.kline('ETH_BTC', 15, 1525760116000, 1525769116000))
+print(bitmart.orderbook('ETH_BTC', 6))
+print(bitmart.trades('ETH_BTC'))
+
+# Authenticated Endpoints
+
+print(bitmart.wallet())
+print(bitmart.place_order('ETH_BTC', 1, 1, 'buy'))
+print(bitmart.cancel_order(564654))
+print(bitmart.cancel_all_order('ETH_BTC', 'buy'))
+print(bitmart.list_orders('ETH_BTC', 0, 0, 100))
+print(bitmart.order_details(123232))
+
+# Service
+
+print(bitmart.precision)
